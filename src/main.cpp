5359a3c4b9f8379c01a8876fa814a99aece39ae6
Naman Siddheshwar <cruisebugati@gmail.com>
2024-09-14 16:08:26 -0700

    has asynch and semi-callbacks

--- src/main.cpp
+++ src/main.cpp
@@ -50,7 +50,7 @@ void on_center_button()
 void initialize()
 {
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
+	pros::lcd::set_text(1, "Hello PROS User4!");
 	pros::lcd::register_btn1_cb(on_center_button);
 
 	// Start odometry localization
@@ -79,74 +79,51 @@ double applyJoystickCurve(double input)
 	return mappedSpeed * 600;
 }
 
-void opcontrol()
-{
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor motor(17, pros::v5::MotorGears::green); // Replace 17 with your motor port
-	pros::adi::Pneumatics adi1('a', false);				// Replace 'a' with your first ADI port
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::Motor motor(17, pros::v5::MotorGears::green); // Replace 17 with your motor port
+pros::adi::Pneumatics adi1('a', false);				// Replace 'a' with your first ADI port
+// pros:adi:Pneumatics adi2('b', false);
+bool mogo_piston_state = false;
+int intake_state = 0; //2 is forward, 1 is reverse, 0 is stop
 
+
+void task_conveyor() {
+	while (true) {
+		if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1))
+		{
+			if (intake_state != 1) {
+				intake_state = 1;
+				motor.move_voltage(-12000);
+			}
+			else {
+				intake_state = 0;
+				motor.move_voltage(0);
+			}
+		}
+
+		// Motor control
+		if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1))
+		{
+			if (intake_state != 2) {
+				intake_state = 2;
+				motor.move_voltage(12000);
+			}
+			else {
+				intake_state = 0;
+				motor.move_voltage(0);
+			}
+		}
+		pros::delay(10);
+	}
+}
+
+void task_clamp() {
 	// Configure ADI devices for piston control
 	adi1.retract(); // Set initial piston position (retracted)
-	bool mogo_piston_state = false;
-	bool intake_state = false;
-	bool reverse_intake_state = false;
-
-	std::uint32_t last_toggle_time = 0; // Store the last toggle time
-
-	// Initialize motor groups
-	pros::MotorGroup leftMotors({-14, 15, 16});
-	pros::MotorGroup rightMotors({-11, 12, -13});
-	leftMotors.set_brake_mode(pros::motor_brake_mode_e::E_MOTOR_BRAKE_BRAKE);
-	rightMotors.set_brake_mode(pros::motor_brake_mode_e::E_MOTOR_BRAKE_BRAKE);
-
-	while (true)
-	{
-		// Motor control
-		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
+	while (true) {
+		if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L2))
 		{
-			if (pros::millis() - last_toggle_time >= 200)
-			{ // Check if 200 milliseconds have passed
-				intake_state = !intake_state;
-				last_toggle_time = pros::millis();
-			}
-		}
-
-		// Motor control
-		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
-		{
-			if (pros::millis() - last_toggle_time >= 200)
-			{ // Check if 200 milliseconds have passed
-				reverse_intake_state = !reverse_intake_state;
-				last_toggle_time = pros::millis();
-			}
-		}
-
-		// Piston control
-		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
-		{
-			if (pros::millis() - last_toggle_time >= 200)
-			{ // Check if 200 milliseconds have passed
-				mogo_piston_state = !mogo_piston_state;
-				last_toggle_time = pros::millis();
-			}
-		}
-
-		if (intake_state)
-		{
-			motor.move_voltage(-12000);
-		}
-		else if (!reverse_intake_state)
-		{
-			motor.move_voltage(0);
-		}
-
-		if (reverse_intake_state)
-		{
-			motor.move_voltage(12000);
-		}
-		else if (!intake_state)
-		{
-			motor.move_voltage(0);
+			mogo_piston_state = !mogo_piston_state;
 		}
 
 		if (mogo_piston_state)
@@ -157,14 +134,61 @@ void opcontrol()
 		{
 			adi1.retract();
 		}
+		pros::delay(10);
+	}
+}
+
+void opcontrol()
+{
+
+	std::uint32_t last_toggle_time = 0; // Store the last toggle time
+
+	// Initialize motor groups
+	pros::MotorGroup leftMotors({-14, 15, 16});
+	pros::MotorGroup rightMotors({-11, 12, -13});
+	leftMotors.set_brake_mode(pros::motor_brake_mode_e::E_MOTOR_BRAKE_BRAKE);
+	rightMotors.set_brake_mode(pros::motor_brake_mode_e::E_MOTOR_BRAKE_BRAKE);
+	
+	pros::Task contask(task_conveyor);
+	pros::Task clatask(task_clamp);
+
+	while (true)
+	{
+		// Motor control
+		// if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1))
+		// {
+		// 	if (intake_state != 1) {
+		// 		intake_state = 1;
+		// 		motor.move_voltage(-12000);
+		// 	}
+		// 	else {
+		// 		intake_state = 0;
+		// 		motor.move_voltage(0);
+		// 	}
+		// }
+
+		// // Motor control
+		// if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1))
+		// {
+		// 	if (intake_state != 2) {
+		// 		intake_state = 2;
+		// 		motor.move_voltage(12000);
+		// 	}
+		// 	else {
+		// 		intake_state = 0;
+		// 		motor.move_voltage(0);
+		// 	}
+		// }
+
+		// Piston control
 
 		// Get joystick inputs
 		double forwardBack = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y); // Forward/backward input
 		double rotation = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);	  // Rotation input
 
 		// Apply joystick curve
-		double leftSpeed = applyJoystickCurve(forwardBack + rotation);
-		double rightSpeed = applyJoystickCurve(forwardBack - rotation);
+		double leftSpeed = applyJoystickCurve(forwardBack - rotation);
+		double rightSpeed = applyJoystickCurve(forwardBack + rotation);
 
 		// Set motor velocities (±600 RPM)
 		leftMotors.move_velocity(leftSpeed);
